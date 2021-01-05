a5913bbdd20147a18fa77a319b5d025c51fa7ac5
Olohundare Nasirudeen <naskkographics@gmail.com>
2021-01-05 12:36:09 +0100

    fixed a typo error

--- src/Pages/OptIn.js
+++ src/Pages/OptIn.js
@@ -1,97 +1,97 @@
-import React, { useState } from 'react';
-import FormBuilder from '../components/Form';
-import Container from '../components/Container';
-import axios from 'axios';
-import { useLocation } from 'react-router-dom';
-import swal from 'sweetalert';
+import React, { useState } from "react";
+import FormBuilder from "../components/Form";
+import Container from "../components/Container";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
+import swal from "sweetalert";
 import { useParams } from "react-router-dom";
 const optinFormData = [
   {
-    name: 'customerType',
-    label: 'what type of customer are you?',
+    name: "customerType",
+    label: "what type of customer are you?",
     validate: {
-      required: 'required',
+      required: "required",
     },
-    type: 'select',
-    selectLabel: 'select one',
-    list: ['new customer', 'returning customer'],
+    type: "select",
+    selectLabel: "select one",
+    list: ["new customer", "returning customer"],
   },
   {
-    name: 'firstName',
-    label: 'First Name',
+    name: "firstName",
+    label: "First Name",
     validate: {
-      required: 'required',
+      required: "required",
     },
-    type: 'text',
+    type: "text",
     dependent: {
-      key: 'customerType',
-      value: 'new customer',
+      key: "customerType",
+      value: "new customer",
     },
   },
   {
-    name: 'middlename',
-    label: 'Middle Name',
+    name: "middlename",
+    label: "Middle Name",
     validate: {
-      required: 'required',
+      required: "required",
     },
-    type: 'text',
+    type: "text",
     dependent: {
-      key: 'customerType',
-      value: 'new customer',
+      key: "customerType",
+      value: "new customer",
     },
   },
   {
-    name: 'lastName',
-    label: 'Last Name',
+    name: "lastName",
+    label: "Last Name",
     validate: {
-      required: 'required',
+      required: "required",
     },
-    type: 'text',
+    type: "text",
     dependent: {
-      key: 'customerType',
-      value: 'new customer',
+      key: "customerType",
+      value: "new customer",
     },
   },
   {
-    name: 'email',
-    label: 'Email',
+    name: "email",
+    label: "Email",
     validate: {
-      required: 'required',
-      email: 'Invalid email address',
+      required: "required",
+      email: "Invalid email address",
     },
-    type: 'email',
+    type: "email",
   },
   {
-    name: 'AccountType',
-    label: 'Select Your Account Type',
+    name: "AccountType",
+    label: "Select Your Account Type",
     validate: {
-      required: 'required',
+      required: "required",
     },
-    type: 'select',
-    selectLabel: 'select one',
-    list: ['AGENT', 'CUSTOMER'],
+    type: "select",
+    selectLabel: "select one",
+    list: ["AGENT", "CUSTOMER"],
   },
   {
-    name: 'policyNumber',
-    label: 'Policy Number',
+    name: "policyNumber",
+    label: "Policy Number",
     validate: {
-      required: 'required',
+      required: "required",
     },
-    type: 'number',
+    type: "number",
     dependent: {
-      key: 'customerType',
-      value: 'returning customer',
+      key: "customerType",
+      value: "returning customer",
     },
   },
   {
-    name: 'wapicPolicy',
+    name: "wapicPolicy",
     label: (
-      <span style={{ fontSize: '14px' }}>
-        I accept coronation insurance{' '}
+      <span style={{ fontSize: "14px" }}>
+        I accept coronation insurance{" "}
         <button
           onClick={() =>
             swal(
-              'Policy, terms and conditions',
+              "Policy, terms and conditions",
               `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 
           Why do we use it?
@@ -102,29 +102,29 @@ const optinFormData = [
           Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
           
           The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`,
-              'info'
+              "info"
             )
           }
           style={{
-            border: 'none',
-            background: 'none',
-            fontSize: '14px',
+            border: "none",
+            background: "none",
+            fontSize: "14px",
             padding: 0,
-            cursor: 'pointer',
-            outline: 'none',
-            borderBottom: '1px solid #883395',
+            cursor: "pointer",
+            outline: "none",
+            borderBottom: "1px solid #883395",
           }}
         >
           policy, terms and conditions
-        </button>{' '}
+        </button>{" "}
         and also I accept to receive messages on whatsapp
       </span>
     ),
 
     validate: {
-      required: 'required',
+      required: "required",
     },
-    type: 'checkbox',
+    type: "checkbox",
   },
 ];
 const OptIn = () => {
@@ -140,25 +140,25 @@ const OptIn = () => {
       const searchParams = new URLSearchParams(location.search);
 
       if (!values.wapicPolicy) {
-        setError('Please accept the policies');
+        setError("Please accept the policies");
         return setLoading(false);
       }
-      const whatsappNo = searchParams.get('whatsapp');
-      const conversationId = searchParams.get('conversationId');
+      const whatsappNo = searchParams.get("whatsapp");
+      const conversationId = searchParams.get("conversationId");
       if (!whatsappNo) return setLoading(false);
       const { data } = await axios.post(
-        'https://wapicbot-api.herokuapp.com/api/auth/optin',
+        "https://wapicbot-api.herokuapp.com/api/auth/optin",
         {
           firstname: values.firstName,
           lastname: values.lastName,
           email: values.email,
           whatsappNo: `+${whatsappNo.trim()}`,
           conversationId,
-          accountType: values.accountType
-          referee: referee !== undefined ? referee : undefined
+          accountType: values.accountType,
+          referee: referee !== undefined ? referee : undefined,
         }
       );
-      window.location = 'https://wa.me/+2348111228899';
+      window.location = "https://wa.me/+2348111228899";
       console.log(data);
       setLoading(false);
     } catch (error) {
@@ -172,11 +172,11 @@ const OptIn = () => {
       <FormBuilder
         error={error}
         loading={loading}
-        title='Whatsapp Opt-in '
-        instruction='Please fill required fields to proceed'
+        title="Whatsapp Opt-in "
+        instruction="Please fill required fields to proceed"
         data={optinFormData}
         action={(values) => {
-          if (values.customerType === 'new customer') submitForm(values);
+          if (values.customerType === "new customer") submitForm(values);
           //alert("submitted data \n" + JSON.stringify(values, null, 2));
         }}
       />
