ebc76384e4fda703467eb9b05bd3916b55bbdb34
adeolaadeoti <adeolaonigegeara@gmail.com>
2021-03-11 01:01:31 +0100

    fix error

--- src/Pages/Claims.js
+++ src/Pages/Claims.js
@@ -96,8 +96,7 @@ function Claims() {
       }
     } catch (error) {
       setLoading(false);
-      if (error.response) setError(error.response.data.message);
-      setError(error.response.data);
+      if (error.response) setError(error.response.data);
       console.log(error.response);
     }
   };
